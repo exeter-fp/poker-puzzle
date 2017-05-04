6373c03d1e7c211755347330454718aab4469b3d
J.A.Henderson <jahenderson777@gmail.com>
2017-05-04 22:49:16 +0100

    slightly less obscure, maybe. with a hint of documentation. and shorter

--- alex-jonathan-clj/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to p54
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- alex-jonathan-clj/src/p54/core.clj
+++ alex-jonathan-clj/src/p54/core.clj
@@ -1,46 +1,40 @@
 (ns p54.core)
 
-(defn score-hand [hand]
-  (let [assoc-cnt (fn [v i cnt]
-                    (let [cnt-i (.indexOf v cnt)]
-                                  (if (pos? cnt-i)
-                                    (-> (assoc v i cnt-i)
-                                        (assoc cnt-i 0))
-                                    v)))
+(defn score [hand]
+  (let [idx #(+ 120 (.indexOf [:pair :2pair :3kind :straight :flush :full-house :4kind :straight-flush] %))
+        assoc-cnt (fn [v c cnt]                             ; find first idx of cnt in the 'score vector'
+                    (let [cnt-i (.indexOf v cnt)]           ; place that idx into appropriate slot identified by c
+                      (if (pos? cnt-i)
+                        (-> (assoc v (idx c) cnt-i)
+                            (assoc cnt-i 0))
+                        v)))
         straight (fn [hand]
                    (->> (apply str (sort (clojure.string/replace hand #"[hcds ]" "")))
                         (.indexOf "23456789ABCDE")
                         inc))
-        clear (fn [v to-clear]
-                (reduce #(assoc %1 (int %2) 0) v to-clear))
-        hand (reduce #(clojure.string/replace %1 (first %2) (second %2))
-                     hand
+        combo (fn [v res a b] 
+                (assoc v (idx res) (pos? (* (nth v (idx a)) (nth v (idx b))))))
+        hand (reduce #(clojure.string/replace %1 (first %2) (second %2)) ; fix hand string so card char value
+                     hand                                                ; is appropriately ascending
                      ["Hh" "Dd" "Cc" "Ss" "JB" "QC" "KD" "AE" "TA"])]
-    (-> (reduce #(update %1 (int %2) inc) (into [] (repeat 128 0)) hand)
-        (assoc-cnt 124 5)
-        (clear "hcds ")
-        (assoc-cnt 120 2)
-        (assoc-cnt 121 2)
-        (assoc-cnt 122 3)
-        (assoc 123 (straight hand))
-        (#(assoc % 125 (pos? (* (nth % 120) (nth % 122)))))
-        (assoc-cnt 126 4)
-        (#(assoc % 127 (pos? (* (nth % 123) (nth % 124)))))
+    (-> (reduce #(update %1 (int %2) inc) (into [] (repeat 128 0)) hand) ; count into respective positions in score vector
+        (assoc-cnt :flush 5)                                             ; check for flush first otherwise suit count will confuse things later
+        ((fn [v] (reduce #(assoc %1 (int %2) 0) v "hcds ")))             ; clear suit count
+        (assoc-cnt :pair 2)
+        (assoc-cnt :2pair 2)                                ; if there's still a pair it's a second pair and higher value
+        (assoc-cnt :3kind 3)
+        (assoc (idx :straight) (straight hand))
+        (combo :full-house :3kind :pair)
+        (assoc-cnt :4kind 4)
+        (combo :straight-flush :straight :flush)
         reverse)))
 
-(defn compare-hands [hand-a hand-b]
-  (loop [a (score-hand hand-a)
-         b (score-hand hand-b)]
-    (let [res (compare (first a) (first b))]
-      (if (= 0 res)
-        (recur (rest a) (rest b))
-        (/ (+ 1 res) 2)))))
-
-
 (defn -main []
-  (->> (slurp "resources/poker.txt")
-       clojure.string/split-lines
-       (map #(compare-hands (subs % 0 14) (subs % 15)))
+  (->> (clojure.string/split-lines (slurp "resources/poker.txt"))
+       (map (fn [line] (if (pos? (some #(when (not= 0 %) %)
+                                       (map compare
+                                            (score (subs line 15))
+                                            (score (subs line 0 14))))) 0 1)))
        (reduce +)
        println))
 
